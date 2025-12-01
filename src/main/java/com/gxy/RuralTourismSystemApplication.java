82b2523478112895651a4ec4e5133b5d1f4dd9fd
GXY123188 <14877493+gxy123188@user.noreply.gitee.com>
2025-12-01 22:35:19 +0800

    初始化框架

---
+++ src/main/java/com/gxy/RuralTourismSystemApplication.java
@@ -0,0 +1,13 @@
+package com.gxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuralTourismSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RuralTourismSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gxy/RuralTourismSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuralTourismSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
